2558f3a471f255855a7c109164384ac28f54a1a1
GroovyArea <095201a@naver.com>
2025-02-15 09:45:19 +0900

    feat : project init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "java-table-tennis"
---
+++ src/test/java/com/tabletennis/JavaTableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabletennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTableTennisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
